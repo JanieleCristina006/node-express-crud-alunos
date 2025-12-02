d215420a3fb2e487c28c7a15b6c06495a6a69743
Janiele <janielecristina45@gmail.com>
2025-12-02 10:33:48 -0300

    config banco

--- db.js
+++ db.js
@@ -5,9 +5,8 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT
 })
-
 module.exports = pool
