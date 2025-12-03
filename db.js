f8f5050704804c2fe2290c9cde2b5984be4e2f9c
Janiele <janielecristina45@gmail.com>
2025-12-03 11:39:08 -0300

    update rename env

--- db.js
+++ db.js
@@ -3,11 +3,13 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 })
+
+
 
 module.exports = pool
